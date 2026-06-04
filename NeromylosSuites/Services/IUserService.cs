39db513abb195ebc826cccde058b6b345c17a876
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-06-04 23:24:04 +0300

    chore(services): complete IUserService

--- NeromylosSuites/Services/IUserService.cs
+++ NeromylosSuites/Services/IUserService.cs
@@ -1,9 +1,16 @@
-﻿using NeromylosSuites.Models;
+﻿using NeromylosSuites.Core;
+using NeromylosSuites.Core.Filters;
+using NeromylosSuites.DTO;
+using NeromylosSuites.Models;
 
 namespace NeromylosSuites.Services
 {
     public interface IUserService
     {
-        Task<User> VerifyAndGetUserAsync();
+        Task<User?> VerifyAndGetUserAsync(UserLoginDTO credentials);
+        Task<UserReadOnlyDTO> GetUserByUsernameAsync(string username);
+        Task<UserReadOnlyDTO> GetUserByIdAsync(int id);
+        Task<PaginatedResult<UserReadOnlyDTO>> GetPaginatedUsersFilteredAsync(int pageNumber, int pageSize, UserFiltersDTO userFiltersDTO);
+        string CreateUserToken(User user);
     }
 }
