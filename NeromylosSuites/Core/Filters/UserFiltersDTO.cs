26566e31c0e32a287e3173eda37afa155bfe1b55
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-06-04 23:19:40 +0300

    chore(filters): add UserFiltersDTO

---
+++ NeromylosSuites/Core/Filters/UserFiltersDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace NeromylosSuites.Core.Filters
+{
+    public class UserFiltersDTO
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Lastname { get; set; }
+        public string? UserRole { get; set; }
+    }
+}
