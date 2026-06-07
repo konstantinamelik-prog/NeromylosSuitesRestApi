a924b7bb07dcb362b9421fb06ff80acf52813d9c
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-06-07 23:02:32 +0300

    chore(DTO): add character restriction at the Password ErrorMessage and refactor setters to initial empty strings

--- NeromylosSuites/DTO/UserLoginDTO.cs
+++ NeromylosSuites/DTO/UserLoginDTO.cs
@@ -6,13 +6,13 @@ namespace NeromylosSuites.DTO
     {
         [Required(ErrorMessage = "The {0} field is required.")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
-        public string? Username { get; init; } = string.Empty;
+        public string? Username { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
         [RegularExpression(@"(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?\W)^.{8,}$", 
-            ErrorMessage = "Password must contain at least 8 characters, including st least one uppercase, one lowercase, " + 
+            ErrorMessage = "Password must contain at least one uppercase, one lowercase, " + 
             "one digit, and one special character")]
-        public string? Password { get; init; } = string.Empty;
+        public string? Password { get; set; }
 
         public bool KeepLoggedIn { get; set; }
     }
