706a45bdd3f20bb8ad6a1d1e7a0216ed411304d6
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-06-07 22:26:48 +0300

    chore(exceptions): add AppException, EntityNotAuthorizedException, EntityNotFoundException

---
+++ NeromylosSuites/Exceptions/EntityNotAuthorizedException.cs
@@ -0,0 +1,12 @@
+﻿namespace NeromylosSuites.Exceptions
+{
+    public class EntityNotAuthorizedException : AppException
+    {
+        private static readonly string DEFAULT_CODE = "Not Authorized";
+
+        public EntityNotAuthorizedException(string code, string message)
+            : base(code + DEFAULT_CODE, message)
+        {
+        }
+    }
+}
---
+++ NeromylosSuites/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace NeromylosSuites.Exceptions
+{
+    public class EntityNotFoundException : AppException
+    {
+        private static readonly string DEFAULT_CODE = "Not Found";
+
+        public EntityNotFoundException(string code, string message) 
+            : base(code + DEFAULT_CODE, message)
+        {
+        }
+    }
+}
