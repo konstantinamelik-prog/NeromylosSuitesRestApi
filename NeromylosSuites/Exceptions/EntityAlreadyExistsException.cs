fe9deba12d488e235284ed5620b74e67db31cf77
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-06-16 23:25:39 +0300

    chore(exceptions): add EntityAlreadyExistsException

---
+++ NeromylosSuites/Exceptions/EntityAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace NeromylosSuites.Exceptions
+{
+    public class EntityAlreadyExistsException : AppException
+    {
+        private static readonly string DEFAULT_CODE = "Already Exists";
+
+        public EntityAlreadyExistsException(string code, string message)
+            : base(code + DEFAULT_CODE, message)
+        {
+        }
+    }
+}
