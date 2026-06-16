2cdd576816256b65ee09310575d0dafcfc433af7
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-06-16 23:22:12 +0300

    chore(DTO): add Special Requests, UserId and VisitorId getter and setter in BookingReadOnlyDTO

--- NeromylosSuites/DTO/BookingReadOnlyDTO.cs
+++ NeromylosSuites/DTO/BookingReadOnlyDTO.cs
@@ -7,7 +7,10 @@
         public DateTime? CheckOut { get; set; }
         public int? NumberOfGuests { get; set; }
         public decimal? TotalPrice { get; set; }
+        public string? SpecialRequests { get; set; }
         public string? Status { get; set; }
+        public int? UserId { get; set; }
+        public int? VisitorId { get; set; }
         public List<string>? RoomNames { get; set; }
     }
 }
