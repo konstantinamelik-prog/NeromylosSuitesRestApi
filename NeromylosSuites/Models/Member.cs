7561367f6b7702c0a98ec8379b88880c6080fbba
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-02 22:58:29 +0300

    chore(models): Add "required" for User entity existence in Member model

--- NeromylosSuites/Models/Member.cs
+++ NeromylosSuites/Models/Member.cs
@@ -3,12 +3,8 @@
 public class Member : BaseEntity
 {
     public int Id { get; set; }
-
     public string? CountryCode { get; set; }
-
     public string PhoneNumber { get; set; } = null!;
-
     public int UserId { get; set; }
-
-    public User User { get; set; } = null!;
+    public required User User { get; set; }
 }
