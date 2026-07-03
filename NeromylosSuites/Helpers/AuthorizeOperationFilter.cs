ef9e317561f4ae2c1a91ee2a453909deccda5bce
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-03 22:45:59 +0300

    chore(helpers): Add Swagger auth filter and global exception handler

---
+++ NeromylosSuites/Helpers/AuthorizeOperationFilter.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace NeromylosSuites.Helpers
+{
+    public class AuthorizeOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var authAttributes = context.MethodInfo
+                .GetCustomAttributes(true)
+                .OfType<AuthorizeAttribute>()
+                .Distinct();
+
+            if (authAttributes.Any())
+            {
+                // Add security requirement
+                operation.Security = new List<OpenApiSecurityRequirement>();
+
+                var roles = context.MethodInfo.GetCustomAttributes(true)
+                    .OfType<AuthorizeAttribute>()
+                    .Where(attr => !string.IsNullOrEmpty(attr.Roles))
+                    .SelectMany(attr => attr.Roles!.Split(','))
+                    .Select(r => r.Trim());
+
+                // Add the security requirment for JWT Bearer with specified roles
+                operation.Security.Add(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Description = "Add token to header",
+                            Name = "Authorization",
+                            Type = SecuritySchemeType.Http,
+                            In = ParameterLocation.Header,
+                            Scheme = JwtBearerDefaults.AuthenticationScheme,
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            }
+                        },
+                        roles.ToList()
+                    }
+                });
+            }
+        }
+    }
+}
