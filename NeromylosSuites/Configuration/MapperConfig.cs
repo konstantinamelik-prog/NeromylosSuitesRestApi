5ef428b4575385c0e3e2714c6d9f97550e4ac877
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-03 22:38:01 +0300

    chore(configuration): Add MapperConfig

---
+++ NeromylosSuites/Configuration/MapperConfig.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using NeromylosSuites.DTO;
+using NeromylosSuites.Models;
+
+namespace NeromylosSuites.Configuration
+{
+    public class MapperConfig : Profile
+    {
+
+        public MapperConfig()
+        {
+            CreateMap<User, UserReadOnlyDTO>()
+                .ForMember(dest => dest.UserRole, opt => opt.MapFrom(src => src.Role.Name));
+
+            CreateMap<MemberSignupDTO, User>()
+                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password))
+                .ForMember(dest => dest.Firstname, opt => opt.MapFrom(src => src.Firstname))
+                .ForMember(dest => dest.Lastname, opt => opt.MapFrom(src => src.Lastname))
+                .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => 2));
+
+            CreateMap<MemberSignupDTO, Member>()
+                .ForMember(dest => dest.CountryCode, opt => opt.MapFrom(src => src.CountryCode))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber));
+
+            CreateMap<Member, MemberReadOnlyDTO>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.User.Username))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.Firstname, opt => opt.MapFrom(src => src.User.Firstname))
+                .ForMember(dest => dest.Lastname, opt => opt.MapFrom(src => src.User.Lastname))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+                .ForMember(dest => dest.CountryCode, opt => opt.MapFrom(src => src.CountryCode));
+        }
+    }
+}
