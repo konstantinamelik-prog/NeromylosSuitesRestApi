0cfe29935d3d63b84ac668f599fefded40dea20a
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-18 19:00:05 +0300

    chore(repositories): Enhance room queries and filtering capabilities - Added using directives for required namespaces. - Introduced GetRoomsByIdsAsync to fetch rooms by IDs. - Modified GetRoomBookingsAsync to filter by room name. - Added GetPaginatedRoomsFilteredAsync for paginated, filtered room retrieval.

--- NeromylosSuites/Repositories/IRoomRepository.cs
+++ NeromylosSuites/Repositories/IRoomRepository.cs
@@ -1,10 +1,15 @@
-﻿using NeromylosSuites.Models;
+﻿using NeromylosSuites.Core;
+using NeromylosSuites.Models;
+using System.Linq.Expressions;
 
 namespace NeromylosSuites.Repositories
 {
     public interface IRoomRepository : IBaseRepository<Room>
     {
-        Task<List<Booking?>> GetRoomBookingsAsync(int roomId);
+        Task<List<Room>> GetRoomsByIdsAsync(List<int> roomIds);
+        Task<List<Booking>> GetRoomBookingsAsync(string roomName);
         Task<List<Room>> GetAvailableRoomsByDateRangeAsync(DateTime checkIn, DateTime checkOut);
+        Task<PaginatedResult<Room>> GetPaginatedRoomsFilteredAsync(
+            int pageNumber, int pageSize, List<Expression<Func<Room, bool>>> predicates);
     }
 }
