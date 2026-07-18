a88ef33563820bf5f5ed9f80f27b1b67326b9875
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-18 19:02:10 +0300

    chore(repositories): register repositories with DI container using AddScoped

--- NeromylosSuites/Repositories/RepositoriesDIExtensions.cs
+++ NeromylosSuites/Repositories/RepositoriesDIExtensions.cs
@@ -6,6 +6,13 @@
         public static IServiceCollection AddRepositories(this IServiceCollection services)
         {
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IMemberRepository, MemberRepository>();
+            services.AddScoped<IVisitorRepository, VisitorRepository>();
+            services.AddScoped<IBookingRepository, BookingRepository>();
+            services.AddScoped<IRoomRepository, RoomRepository>();
+            services.AddScoped<ISeasonalPricesRepository, SeasonalPricesRepository>();
+
             return services;
         }
     }
