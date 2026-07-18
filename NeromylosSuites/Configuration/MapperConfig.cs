cf331cd88eb8dec24ceb9b62a4eac10356726cb7
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-18 18:41:25 +0300

    chore(config): - Change default user RoleId mapping from 2 to 1 - Simplify Member to MemberReadOnlyDTO mapping - Add profiles for Booking, Visitor, and Room DTOs - Mapped Booking.Rooms to RoomNames in BookingReadOnlyDTO

--- NeromylosSuites/Configuration/MapperConfig.cs
+++ NeromylosSuites/Configuration/MapperConfig.cs
@@ -18,20 +18,38 @@ namespace NeromylosSuites.Configuration
                 .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password))
                 .ForMember(dest => dest.Firstname, opt => opt.MapFrom(src => src.Firstname))
                 .ForMember(dest => dest.Lastname, opt => opt.MapFrom(src => src.Lastname))
-                .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => 2));
+                .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => 1));
 
             CreateMap<MemberSignupDTO, Member>()
                 .ForMember(dest => dest.CountryCode, opt => opt.MapFrom(src => src.CountryCode))
                 .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber));
 
             CreateMap<Member, MemberReadOnlyDTO>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.User.Username))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
                 .ForMember(dest => dest.Firstname, opt => opt.MapFrom(src => src.User.Firstname))
-                .ForMember(dest => dest.Lastname, opt => opt.MapFrom(src => src.User.Lastname))
-                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
-                .ForMember(dest => dest.CountryCode, opt => opt.MapFrom(src => src.CountryCode));
+                .ForMember(dest => dest.Lastname, opt => opt.MapFrom(src => src.User.Lastname));
+
+            CreateMap<Booking, BookingReadOnlyDTO>()
+                .ForMember(dest => dest.RoomNames, 
+                    opt => opt.MapFrom(src => src.Rooms.Select(r => r.Name).ToList()));
+
+            CreateMap<CreateBookingDTO, Booking>()
+                .ForMember(dest => dest.CheckIn, opt => opt.MapFrom(src => src.CheckIn!.Value))
+                .ForMember(dest => dest.CheckOut, opt => opt.MapFrom(src => src.CheckOut!.Value))
+                .ForMember(dest => dest.NumberOfGuests, opt => opt.MapFrom(src => src.NumberOfGuests!.Value))
+                .ForMember(dest => dest.SpecialRequests, opt => opt.MapFrom(src => src.SpecialRequests))
+                .ForMember(dest => dest.Rooms, opt => opt.Ignore())
+                .ForMember(dest => dest.Status, opt => opt.Ignore())
+                .ForMember(dest => dest.TotalPrice, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.VisitorId, opt => opt.Ignore());
+
+            CreateMap<Visitor, VisitorReadOnlyDTO>();
+
+            CreateMap<CreateVisitorDTO, Visitor>();
+
+            CreateMap<Room, RoomReadOnlyDTO>();
         }
     }
 }
