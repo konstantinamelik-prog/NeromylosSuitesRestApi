489fa86856e3fbc08215f8dbb124cb93e380ad12
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-18 18:48:09 +0300

    chore(DTO): refactor CreateBookingDTO to collect guest details

--- NeromylosSuites/DTO/CreateBookingDTO.cs
+++ NeromylosSuites/DTO/CreateBookingDTO.cs
@@ -13,20 +13,27 @@ namespace NeromylosSuites.DTO
         [Required(ErrorMessage = "The {0} field is required.")]
         public int? NumberOfGuests { get; set; }
 
-        [Required(ErrorMessage = "The {0} field is required.")]
-        public decimal? TotalPrice { get; set; }
-
         [StringLength(255, ErrorMessage = "Special Requests must be maximum 255 characters.")]
         public string? SpecialRequests { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
-        public string? Status { get; set; } = "PENDING";
+        [StringLength(50, MinimumLength = 2)]
+        public string? Firstname { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
-        public int? UserId { get; set; }
+        [StringLength(50, MinimumLength = 2)]
+        public string? Lastname { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
-        public int? VisitorId { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(15, MinimumLength = 10)]
+        public string? PhoneNumber { get; set; }
+
+        [StringLength(2, MinimumLength = 2)]
+        public string? CountryCode { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
         public List<int>? RoomIds { get; set; } 
