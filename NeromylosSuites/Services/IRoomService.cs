b28641ce8c13d7a250c7d51ae3a9d269fb8fa43c
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-18 19:05:06 +0300

    feat(services): Add IRoomService and RoomService

---
+++ NeromylosSuites/Services/IRoomService.cs
@@ -0,0 +1,15 @@
+﻿using NeromylosSuites.Core;
+using NeromylosSuites.Core.Filters;
+using NeromylosSuites.DTO;
+using NeromylosSuites.Models;
+
+namespace NeromylosSuites.Services
+{
+    public interface IRoomService
+    {
+        Task<List<BookingReadOnlyDTO>> GetRoomBookingsAsync(string roomName);
+        Task<List<RoomReadOnlyDTO>> GetAvailableRoomsByDateRangeAsync(DateTime checkIn, DateTime checkOut);
+        Task<PaginatedResult<RoomReadOnlyDTO>> GetPaginatedRoomsFilteredAsync(
+            int pageNumber, int pageSize, RoomFiltersDTO roomFiltersDTO);
+    }
+}
---
+++ NeromylosSuites/Services/RoomService.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using NeromylosSuites.Core;
+using NeromylosSuites.Core.Filters;
+using NeromylosSuites.DTO;
+using NeromylosSuites.Models;
+using NeromylosSuites.Repositories;
+using System.Linq.Expressions;
+
+namespace NeromylosSuites.Services
+{
+    public class RoomService : IRoomService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<RoomService> _logger;
+
+        public RoomService(IUnitOfWork unitOfWork, IMapper mapper, ILogger<RoomService> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<List<RoomReadOnlyDTO>> GetAvailableRoomsByDateRangeAsync(DateTime checkIn, DateTime checkOut)
+        {
+            var availableRooms = await _unitOfWork.RoomRepository.GetAvailableRoomsByDateRangeAsync(checkIn, checkOut);
+
+            return _mapper.Map<List<RoomReadOnlyDTO>>(availableRooms);
+        }
+
+        public async Task<List<BookingReadOnlyDTO>> GetRoomBookingsAsync(string roomName)
+        {
+            var bookings = await _unitOfWork.RoomRepository.GetRoomBookingsAsync(roomName);
+
+            return _mapper.Map<List<BookingReadOnlyDTO>>(bookings);
+        }
+
+        public async Task<PaginatedResult<RoomReadOnlyDTO>> GetPaginatedRoomsFilteredAsync(
+            int pageNumber, int pageSize, RoomFiltersDTO roomFiltersDTO)
+        {
+            List<Expression<Func<Room, bool>>> predicates = [];
+
+            if (roomFiltersDTO.RoomNumber.HasValue)
+            {
+                predicates.Add(r => r.RoomNumber == roomFiltersDTO.RoomNumber);
+            }
+            if (!string.IsNullOrEmpty(roomFiltersDTO.Name))
+            {
+                predicates.Add(r => r.Name == roomFiltersDTO.Name);
+            }
+            if (!string.IsNullOrEmpty(roomFiltersDTO.Status))
+            {
+                predicates.Add(r => r.Status == roomFiltersDTO.Status);
+            }
+
+            var result = await _unitOfWork.RoomRepository.GetPaginatedRoomsFilteredAsync(pageNumber, pageSize,
+                predicates);
+
+            var dtoResult = new PaginatedResult<RoomReadOnlyDTO>()
+            {
+                Data = _mapper.Map<List<RoomReadOnlyDTO>>(result.Data),
+                TotalRecords = result.TotalRecords,
+                PageNumber = result.PageNumber,
+                PageSize = result.PageSize
+            };
+
+            _logger.LogInformation("Retrieved {Count} rooms", dtoResult.Data.Count);
+            return dtoResult;
+        }
+    }
+}
