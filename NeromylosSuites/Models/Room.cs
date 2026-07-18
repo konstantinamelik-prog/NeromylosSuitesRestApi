6b0e5d22ba503f4cbc2560cc730760e4b5d939cf
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-18 18:52:51 +0300

    chore(models): remove Price Property from Room

--- NeromylosSuites/Models/Room.cs
+++ NeromylosSuites/Models/Room.cs
@@ -9,7 +9,6 @@
         public int MaxOccupancy { get; set; } = 0!;
         public string Status { get; set; } = null!;
         public string ImageUrl { get; set; } = null!;
-        public decimal? Price { get; set; }
         public ICollection<Booking>? Bookings { get; set; } = new List<Booking>();
         public ICollection<SeasonalPrice>? SeasonalPrices { get; set; } = new List<SeasonalPrice>();
     }
