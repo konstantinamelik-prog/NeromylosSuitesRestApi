750ccf91c1a08f436abf66f2dbeeaffb11d531b2
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-27 21:12:00 +0300

    fix(models): Add Id property to Role class

--- NeromylosSuites/Models/Role.cs
+++ NeromylosSuites/Models/Role.cs
@@ -2,6 +2,7 @@
 {
     public class Role
     {
+        public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
         public ICollection<User> Users { get; set; } = new List<User>();
