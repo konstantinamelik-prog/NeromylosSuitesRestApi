01f8708024dc518b7859015b9c4399c0296d9951
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-27 13:55:22 +0300

    style(models): remove empty row at ErrorViewModel

--- NeromylosSuites/Models/ErrorViewModel.cs
+++ NeromylosSuites/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace NeromylosSuites.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
